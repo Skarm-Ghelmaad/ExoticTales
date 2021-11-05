319dcdd11e5357e6b129fead6f8f0ca413c8abe1
Skarm-Ghelmaad <83535483+Skarm-Ghelmaad@users.noreply.github.com>
2021-11-05 21:59:44 +0100

    Various adjustments

--- ExoticTales/Config/Blueprints.cs
+++ ExoticTales/Config/Blueprints.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using HarmonyLib;
+﻿using HarmonyLib;
 using Kingmaker.Blueprints;
 using Kingmaker.Blueprints.JsonSystem;
 using Kingmaker.Utility;
 using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 
 namespace ExoticTales.Config
 {
